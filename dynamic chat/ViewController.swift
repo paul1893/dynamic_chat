d815b1329d7428b8afd1f0692922193682d07e1f
Paul <pbancarel@octo.com>
2019-07-31 13:51:28 +0200

    Initial Commit

---
+++ dynamic chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dynamic chat
+//
+//  Created by Paul Bancarel on 31/07/2019.
+//  Copyright © 2019 Paul Bancarel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
